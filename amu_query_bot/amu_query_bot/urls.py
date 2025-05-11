9d44dd3d0ff3efb637b389328d4ebb71816349d0
Tabassum Yunus <tabassumyunus09@gmail.com>
2025-05-11 11:10:09 +0530

    add : starter

--- amu_query_bot/amu_query_bot/urls.py
+++ amu_query_bot/amu_query_bot/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("chat/", include("core.urls")),
-    path("users/", include("users.urls")),
+    # path("chat/", include("core.urls")),
+    # path("users/", include("users.urls")),
 ]
