b3eeb1c68df3f318f2b7339bc952d99c5c2aebde
Tabassum-Yunus <tabassumyunus09@gmail.com>
2025-05-29 16:41:05 +0530

    add: export resources functionality

--- amu_query_bot/main/urls.py
+++ amu_query_bot/main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('feedback/', views.submit_feedback, name='submit_feedback'),
+    path('crawl-resources/', views.crawl_resources, name='crawl-resources'),
 ]
